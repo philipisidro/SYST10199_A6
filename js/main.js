497b6509fa7586b45d624acbef4a8872e3716820
philipisidro <philipisidro@gmail.com>
2022-04-17 13:41:05 -0400

    push

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-//change
-const submitButton = document.getElementById('submitBtn');
-const viewTableButton = document.getElementById('viewTableBtn');
+const insert = document.getDocumentById('insert');
+const upddate = document.getElementById('update');
+const deleteRow = document.getElementById('deleteRow');
+const viewTable = document.getElementById('viewTable');
 
-const insertOpt = document.getElementById('#insert').checked;
-const updateOpt = document.getElementById('#updte').checked;
-const deleteOpt = document.getElementById('#delete').checked;
-
-
-
-submitButton.addEventListener('click', () => {
-    if (insertOpt){
-        console.log("you going to insert");
-    }else if(updateOpt){
-        console.log("you going to update");
-    }else if(deleteOpt){
-        console.log("you going to delete");
-    }else {
-        console.log("please select an option");
-    }
-});
